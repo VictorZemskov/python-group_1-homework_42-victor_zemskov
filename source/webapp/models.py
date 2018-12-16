2513734c13096f2756b5a440412625daae1937f4
Victor <vzemskov090583@gmail.com>
2018-12-16 16:07:45 +0600

    Added model 'User'

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    nickname = models.CharField(max_length=50, verbose_name='Никнейм')
+    name = models.CharField(max_length=50, verbose_name='Имя')
+    surname = models.CharField(max_length=50, verbose_name='Фамилия')
+
+    def __str__(self):
+        return self.nickname
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
