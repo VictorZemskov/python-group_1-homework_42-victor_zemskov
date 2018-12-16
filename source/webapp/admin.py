afb11ff61ba4c67736c39ff42cc6b7e5f33c7e5f
Victor <vzemskov090583@gmail.com>
2018-12-16 16:58:46 +0600

    Added model 'Article' and field favorites in model 'User'

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from webapp.models import User
+from webapp.models import User, Article
 
 # Register your models here.
 
 admin.site.register(User)
+admin.site.register(Article)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,6 +6,7 @@ class User(models.Model):
     nickname = models.CharField(max_length=50, verbose_name='Никнейм')
     name = models.CharField(max_length=50, verbose_name='Имя')
     surname = models.CharField(max_length=50, verbose_name='Фамилия')
+    favorites = models.ManyToManyField('Article', blank=True, related_name='favored_by', verbose_name='Избранное')
 
     def __str__(self):
         return self.nickname
@@ -13,3 +14,15 @@ class User(models.Model):
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
+
+class Article(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Заголовок')
+    content = models.TextField(max_length=2000, verbose_name='Содержание статьи')
+    author = models.ForeignKey('User', on_delete=models.PROTECT, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
