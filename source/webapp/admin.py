622d438ec2d55a0033e28c3adc4e6f33b69a90e8
Victor <vzemskov090583@gmail.com>
2018-12-16 17:11:24 +0600

    Added class UserAdmin in admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from webapp.models import User, Article
 
 # Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    filter_vertical = ('favorites',)
 
-admin.site.register(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(Article)
