9441badc40bf18b9ee2fa2293c762f375a711524
Victor <vzemskov090583@gmail.com>
2018-12-20 13:25:57 +0600

    Added class ArticleForm in 'forms.py'

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from webapp.views import ArticleListView, ArticleDetailView, UserListView, UserDetailView, ArticleCreateView
+from webapp.views import ArticleListView, ArticleDetailView, UserListView, UserDetailView, ArticleCreateView, ArticleUpdateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,5 +23,6 @@ urlpatterns = [
     path('articles/<int:pk>', ArticleDetailView.as_view(), name='article_detail'),
     path('users', UserListView.as_view(), name='user_list'),
     path('user/<int:pk>', UserDetailView.as_view(), name='user_detail'),
-    path('articles/create', ArticleCreateView.as_view, name='article_create'),
+    path('articles/create', ArticleCreateView.as_view(), name='article_create'),
+    path('articles/<int:pk>/update', ArticleUpdateView.as_view(), name='article_update'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, FormView, CreateView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView
 from webapp.models import User, Article, Comment, Ratting
 from webapp.forms import ArticleSearchForm, ArticleForm
 from django.urls import reverse_lazy
@@ -36,5 +36,10 @@ class ArticleCreateView(CreateView):
     form_class = ArticleForm
     success_url = reverse_lazy('article_list')
 
+class ArticleUpdateView(UpdateView):
+    model = Article
+    template_name = 'article_update.html'
+    form_class = ArticleForm
+    success_url = reverse_lazy('article_list')
 
 
